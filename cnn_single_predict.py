086d1be476cda775adefd6268f16030bd67e6c4b
rohan sharma <rohan.sharma12@student.amity.edu>
2019-07-03 20:43:36 +0530

    change single_predict

--- cnn_single_predict.py
+++ cnn_single_predict.py
@@ -3,16 +3,18 @@ import numpy as np
 from keras.preprocessing import image
 
 def prediction():
-    classifier = load_model('cnn_model_3l_drp_128size.h5')
+    classifier = load_model('recycle_model.h5')
     
     test_image = image.load_img('path', target_size = (128, 128))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis = 0)
     result = classifier.predict(test_image)
     pred = {'class' : 'D'}
+    #0 is Organic
+    #1 is Recyclable (Non Organic)
     if result[0][0] == 1:
-        pred['class'] = 'Dog'
+        pred['class'] = 'Recyclable'
     else:
-        pred['class'] = 'Cat'
+        pred['class'] = 'Organic'
 
     return pred
